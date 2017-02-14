c8ef8d150c835d7ebe762384ed946b5cca9ef4c3
arbianchi <adina.r.bianchi@gmail.com>
2017-02-14 15:28:59 -0500

    fix bootstrap css error

--- src/containers/NewRequest.js
+++ src/containers/NewRequest.js
@@ -6,11 +6,11 @@ import DateTimePicker from 'react-datetimepicker-bootstrap';
 
 
 class NewRequest extends Component {
-  
+
   static contextTypes = {
     router: PropTypes.object
   };
-  
+
   onSubmit(props) {
     const token = localStorage.getItem('token');
 
@@ -19,36 +19,35 @@ class NewRequest extends Component {
      .then(() => {
        this.context.router.push('/requests/new');
      })
-     .catch( err => console.log( err )); 
+     .catch( err => console.log( err ));
   }
-  
+
   render() {
-    
+
     const { fields: { pickup_address, dropoff_address, arrival_datetime }, handleSubmit } = this.props;
 
     return(
       <div>
         <h1>Request a Ride</h1>
-		  	  <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-        <div className="container">
-
-          <div className="row">
-        		<div className="col-md-4 col-md-offset-4">
-          		<div className="panel panel-default">
-      			  	<div className="panel-heading">
-      			    	<h3 className="panel-title">Request a Ride</h3>
-      			 	  </div>
-      			  	<div className="panel-body">
-                    <AddressForm {...pickup_address} />
-                    <AddressForm {...dropoff_address} />
-      			    </div>
-        			</div>
-              <div className="m-b-l">
-                <DateTimePicker className="form-control" placeholder="Arrival Date and Time" id="datetimepicker" {...arrival_datetime}/>
-              </div>
-        		</div>
-        	</div>
-        </div>
+	  	  <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+          <div className="container">
+            <div className="row">
+          		<div className="col-md-4 col-md-offset-4">
+            		<div className="panel panel-default">
+        			  	<div className="panel-heading">
+        			    	<h3 className="panel-title">Request a Ride</h3>
+        			 	  </div>
+        			  	<div className="panel-body">
+                      <AddressForm {...pickup_address} />
+                      <AddressForm {...dropoff_address} />
+        			    </div>
+          			</div>
+                <div className="m-b-l">
+                  <DateTimePicker className="form-control" placeholder="Arrival Date and Time" id="datetimepicker" {...arrival_datetime}/>
+                </div>
+          		</div>
+          	</div>
+          </div>
         	<button type="submit" className="btn btn-primary">Submit Request</button>
       	</form>
       </div>
