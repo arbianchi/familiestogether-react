b111953065fc6b6cb5855979f4096022ff64ffac
arbianchi <adina.r.bianchi@gmail.com>
2017-03-09 09:21:05 -0500

    fix new request page

--- src/containers/NewRequest.js
+++ src/containers/NewRequest.js
@@ -26,71 +26,68 @@ class NewRequest extends Component {
     const { fields: { pickup_street, pickup_city, pickup_zipcode, dropoff_street, dropoff_city, dropoff_zipcode, arrival_datetime }, handleSubmit } = this.props;
 
     return(
-      <div>
-        <h1>Request a Ride</h1>
-	  	  <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-          <div className="container">
+      <div className='text-center'>
+        <div className="container">
+          <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
             <div className="row">
-          		<div className="col-md-4 col-md-offset-4">
-            		<div className="panel panel-default">
-        			  	<div className="panel-heading">
-        			    	<h3 className="panel-title">Request a Ride</h3>
-        			 	  </div>
-        			  	<div className="panel-body">
+              <div className="col-md-5 col-md-offset-4">
+                <div className="panel panel-default">
+                  <div className="panel-heading">
+                    <h2 className="panel-title">Request a Ride</h2>
+                  </div>
+                  <div className="panel-body">
                     <div className="container">
                       <div className="row">
-                    		<div className="col-md-4 col-md-offset-4">
-                      		<div className="panel panel-default">
-                  			  	<div className="panel-heading">
-                  			    	<h3 className="panel-title">Request a Ride</h3>
-                  			 	  </div>
-                  			  	<div className="panel-body">
-                			    	  	<div className="form-group">
-                			    		    <input className="form-control" placeholder="Street" type="text" {...pickup_street} />
-                  			    		</div>
-                			    	  	<div className="form-group">
-                			    		    <input className="form-control" placeholder="City" type="text" {...pickup_city} />
-                  			    		</div>
-                			    	  	<div className="form-group">
-                			    		    <input className="form-control" placeholder="Zipcode" type="text" {...pickup_zipcode} />
-                  			    		</div>
-                  			    </div>
-                    			</div>
-                    		</div>
-                    	</div>
-                  </div>
+                        <div className="col-md-4 text-center">
+                          <div className="panel panel-default">
+                            <div className="panel-heading">
+                              <h3 className="panel-title">Pickup Address</h3>
+                            </div>
+                            <div className="panel-body">
+                              <div className="form-group">
+                                <input className="form-control" placeholder="Street" type="text" {...pickup_street} />
+                              </div>
+                              <div className="form-group">
+                                <input className="form-control" placeholder="City" type="text" {...pickup_city} />
+                              </div>
+                              <div className="form-group">
+                                <input className="form-control" placeholder="Zipcode" type="text" {...pickup_zipcode} />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                     <div className="container">
                       <div className="row">
-                    		<div className="col-md-4 col-md-offset-4">
-                      		<div className="panel panel-default">
-                  			  	<div className="panel-heading">
-                  			    	<h3 className="panel-title">Dropoff Address</h3>
-                  			 	  </div>
-                  			  	<div className="panel-body">
-                			    	  	<div className="form-group">
-                			    		    <input className="form-control" placeholder="Street" type="text" {...dropoff_street} />
-                  			    		</div>
-                			    	  	<div className="form-group">
-                			    		    <input className="form-control" placeholder="City" type="text" {...dropoff_city} />
-                  			    		</div>
-                			    	  	<div className="form-group">
-                			    		    <input className="form-control" placeholder="Zipcode" type="text" {...dropoff_zipcode} />
-                  			    		</div>
-                  			    </div>
-                    			</div>
-                    		</div>
-                    	</div>
+                        <div className="col-md-4 text-center">
+                          <div className="panel panel-default">
+                            <div className="panel-heading">
+                              <h3 className="panel-title">Dropoff Address</h3>
+                            </div>
+                            <div className="panel-body">
+                              <div className="form-group">
+                                <input className="form-control" placeholder="Street" type="text" {...dropoff_street} />
+                              </div>
+                              <div className="form-group">
+                                <input className="form-control" placeholder="City" type="text" {...dropoff_city} />
+                              </div>
+                              <div className="form-group">
+                                <input className="form-control" placeholder="Zipcode" type="text" {...dropoff_zipcode} />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                      <DateTimePicker className="form-control" placeholder="Arrival Date and Time" id="datetimepicker" {...arrival_datetime}/>
                   </div>
-                <div className="m-b-l">
-                  <DateTimePicker className="form-control" placeholder="Arrival Date and Time" id="datetimepicker" {...arrival_datetime}/>
                 </div>
-        			    </div>
-          			</div>
-          		</div>
-          	</div>
-          </div>
-        	<button type="submit" className="btn btn-primary">Submit Request</button>
-      	</form>
+                      <button type="submit" className="btn btn-primary">Submit Request</button>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
@@ -98,5 +95,4 @@ class NewRequest extends Component {
 
 export default reduxForm({
   form: 'RequestForm',
-  fields: ['pickup_street', 'pickup_city', 'pickup_zipcode', 'dropoff_street', 'dropoff_city', 'dropoff_zipcode', 'arrival_datetime']}
-, null, null)(NewRequest);
+  fields: ['pickup_street', 'pickup_city', 'pickup_zipcode', 'dropoff_street', 'dropoff_city', 'dropoff_zipcode', 'arrival_datetime']})(NewRequest);
